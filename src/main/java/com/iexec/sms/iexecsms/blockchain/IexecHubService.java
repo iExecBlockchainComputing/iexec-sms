a4fad873ad0817e63d830ddcaac587bb442d1a95
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-07-31 12:14:19 +0200

    Added blockchain & authorization services

---
+++ src/main/java/com/iexec/sms/iexecsms/blockchain/IexecHubService.java
@@ -0,0 +1,26 @@
+package com.iexec.sms.iexecsms.blockchain;
+
+
+import com.iexec.common.chain.IexecHubAbstractService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+public class IexecHubService extends IexecHubAbstractService {
+
+    private final CredentialsService credentialsService;
+    private Web3jService web3jService;
+
+    @Autowired
+    public IexecHubService(CredentialsService credentialsService,
+                           Web3jService web3jService,
+                           BlockchainConfig blockchainConfig) {
+        super(credentialsService.getCredentials(), web3jService, blockchainConfig.getHubAddress());
+        this.credentialsService = credentialsService;
+        this.web3jService = web3jService;
+    }
+
+}
--- src/main/java/com/iexec/sms/iexecsms/secret/SecretPayload.java
+++ src/main/java/com/iexec/sms/iexecsms/secret/SecretPayload.java
@@ -9,17 +9,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SecretPayload {
 
-    private String symmetricKey;
-    private String credentials;
+    private String symmetricKey; //(Kd, Kb, Ke)
+    private String beneficiaryCredentials; //dropbox, AWS, TODO think about
     // ...
-
 
     SecretPayload updateSecretPayloadFields(SecretPayload newSecretPayload) {
         if (!newSecretPayload.getSymmetricKey().isEmpty()) {
             this.setSymmetricKey(newSecretPayload.getSymmetricKey());
         }
-        if (!newSecretPayload.getCredentials().isEmpty()) {
-            this.setCredentials(newSecretPayload.getCredentials());
+        if (!newSecretPayload.getBeneficiaryCredentials().isEmpty()) {
+            this.setBeneficiaryCredentials(newSecretPayload.getBeneficiaryCredentials());
         }
         return this;
     }
