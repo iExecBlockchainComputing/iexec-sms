06bc047e69fe03b4356d7ea7f1ec6248bc4d41dd
Ugo Plouviez <up@iex.ec>
2019-08-12 14:47:28 +0200

    Modify PalaemonConfig file generation

--- src/main/java/com/iexec/sms/iexecsms/PalaemonHelperService.java
+++ src/main/java/com/iexec/sms/iexecsms/PalaemonHelperService.java
@@ -1,19 +1,25 @@
 package com.iexec.sms.iexecsms;
 
-import com.iexec.common.chain.ChainDataset;
 import com.iexec.common.chain.ChainDeal;
 import com.iexec.common.chain.ChainTask;
 import com.iexec.common.utils.BytesUtils;
 import com.iexec.sms.iexecsms.blockchain.IexecHubService;
 import org.apache.commons.lang.RandomStringUtils;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.stereotype.Service;
 
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class PalaemonHelperService {
+
+    private String PALAEMON_CONFIG_FILE_WITH_DATASET = "src/main/resources/palaemonConfTemplateWithDataset.vm";
+    private String PALAEMON_CONFIG_FILE_WITHOUT_DATASET = "src/main/resources/palaemonConfTemplateWithoutDataset.vm";
 
     private String SESSIONS_ID_PROPERTY = "SESSION_ID";
     private String COMMAND_PROPERTY = "PROPERTY";
@@ -23,6 +29,8 @@ public class PalaemonHelperService {
     private String ENCLAVE_KEY_PROPERTY = "ENCLAVE_KEY";
     private String TASK_ID_PROPERTY = "TASK_ID";
     private String WORKER_ADDRESS_PROPERTY = "WORKER_ADDRESS";
+    private String DATA_FSPF_TAG_PROPERTY = "DATA_FSPF_TAG";
+    private String DATA_FSPF_KEY_PROPERTY = "DATA_FSPF_KEY";
 
     private IexecHubService iexecHubService;
 
@@ -30,14 +38,14 @@ public class PalaemonHelperService {
         this.iexecHubService = iexecHubService;
     }
 
-    public Map<String, String> getTokenList(String taskId, String workerAddress) throws Exception {
+    private Map<String, String> getTokenList(String taskId, String workerAddress) throws Exception {
         Optional<ChainTask> oChainTask = iexecHubService.getChainTask(taskId);
-        if(!oChainTask.isPresent()){
+        if (!oChainTask.isPresent()) {
             return new HashMap<>();
         }
         ChainTask chainTask = oChainTask.get();
         Optional<ChainDeal> oChainDeal = iexecHubService.getChainDeal(chainTask.getDealid());
-        if(!oChainDeal.isPresent()) {
+        if (!oChainDeal.isPresent()) {
             return new HashMap<>();
         }
         ChainDeal chainDeal = oChainDeal.get();
@@ -63,5 +71,29 @@ public class PalaemonHelperService {
         tokens.put(WORKER_ADDRESS_PROPERTY, workerAddress);
         // TODO: DATASET tokens missing
         return tokens;
+    }
+
+    public String getPalaemonConfigurationFile(String taskId, String workerAddress) throws Exception {
+
+        // Palaemon file should be generated and a call to the CAS with this file should happen here.
+        Map<String, String> tokens = getTokenList(taskId, workerAddress);
+
+        VelocityEngine ve = new VelocityEngine();
+        ve.init();
+
+        Template t;
+        if(tokens.containsKey(DATA_FSPF_KEY_PROPERTY) && tokens.containsKey(DATA_FSPF_TAG_PROPERTY)) {
+            t = ve.getTemplate(PALAEMON_CONFIG_FILE_WITH_DATASET);
+        } else {
+            t = ve.getTemplate(PALAEMON_CONFIG_FILE_WITHOUT_DATASET);
+        }
+        VelocityContext context = new VelocityContext();
+        // copy all data from the tokens into context
+        tokens.forEach(context::put);
+
+        StringWriter writer = new StringWriter();
+        t.merge(context, writer);
+
+        return writer.toString();
     }
 }
--- src/main/java/com/iexec/sms/iexecsms/session/SessionController.java
+++ src/main/java/com/iexec/sms/iexecsms/session/SessionController.java
@@ -5,25 +5,16 @@ import com.iexec.common.sms.SmsRequest;
 import com.iexec.common.sms.scone.SconeSecureSessionResponse;
 import com.iexec.sms.iexecsms.PalaemonHelperService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.StringWriter;
-import java.util.Map;
 
 @Slf4j
 @RestController
 public class SessionController {
 
     private PalaemonHelperService palaemonHelperService;
-
-    private String PALAEMON_CONFIG_FILE_WITH_DATASET = "src/main/resources/palaemonConfTemplateWithDataset.vm";
-    private String PALAEMON_CONFIG_FILE_WITHOUT_DATASET = "src/main/resources/palaemonConfTemplateWithoutDataset.vm";
 
     public SessionController(PalaemonHelperService palaemonHelperService) {
         this.palaemonHelperService = palaemonHelperService;
@@ -32,30 +23,13 @@ public class SessionController {
     @PostMapping("/sessions/generate")
     public ResponseEntity<SconeSecureSessionResponse> generateSecureSession(@RequestBody SmsRequest smsRequest) throws Exception {
 
-        // depends with dataSet or not
-
-        // TODO contact the CAS and perform whatever is needed
         String taskId = smsRequest.getSmsSecretRequestData().getChainTaskId();
         String workerAddress = smsRequest.getSmsSecretRequestData().getWorkerAddress();
-
-        // Palaemon file should be generated and a call to the CAS with this file should happen here.
-        Map<String, String> tokens =palaemonHelperService.getTokenList(taskId, workerAddress);
-
-        VelocityEngine ve = new VelocityEngine();
-        ve.init();
-
-        Template t = ve.getTemplate(PALAEMON_CONFIG_FILE_WITHOUT_DATASET);
-        VelocityContext vc = new VelocityContext();
-        // copy all data from the tokens into vc
-        tokens.forEach(vc::put);
-
-        StringWriter sw = new StringWriter();
-        t.merge(vc, sw);
+        String configFile = palaemonHelperService.getPalaemonConfigurationFile(taskId, workerAddress);
+        System.out.println(configFile);
 
         // TODO: send all to the CAS to generate the session
-        System.out.println(sw);
-        SconeSecureSessionResponse data;
-
+        
         return ResponseEntity.notFound().build();
     }
 }
