da902e8e749fb381506bedfc32ff86ea384bd58d
Ugo Plouviez <up@iex.ec>
2019-08-13 14:51:48 +0200

    Add authorization checks in getSecret

--- src/main/java/com/iexec/sms/iexecsms/attestation/AttestationService.java
+++ src/main/java/com/iexec/sms/iexecsms/attestation/AttestationService.java
@@ -20,17 +20,17 @@ public class AttestationService {
     Optional<Attestation> getOrCreate(String taskId) {
         // if existing returns from the db
         Optional<Attestation> oAttestation = attestationRepository.findByTaskId(taskId);
-        if(oAttestation.isPresent()) {
+        if (oAttestation.isPresent()) {
             return oAttestation;
         }
 
         // otherwise create it
         try {
-            Credentials credentials = Credentials.create(Keys.createEcKeyPair());
-            return Optional.of(Attestation.builder()
-                    .credentials(credentials)
+            Attestation attest = attestationRepository.save(Attestation.builder()
+                    .credentials(Credentials.create(Keys.createEcKeyPair()))
                     .taskId(taskId)
                     .build());
+            return Optional.of(attest);
         } catch (Exception e) {
             log.error("Couldn't create credentials [exception:{}]", e.getMessage());
             return Optional.empty();
--- src/main/java/com/iexec/sms/iexecsms/secret/SecretController.java
+++ src/main/java/com/iexec/sms/iexecsms/secret/SecretController.java
@@ -1,8 +1,13 @@
 package com.iexec.sms.iexecsms.secret;
 
 
+import com.iexec.common.security.Signature;
 import com.iexec.common.sms.SmsRequest;
+import com.iexec.common.sms.SmsRequestData;
+import com.iexec.sms.iexecsms.authorization.Authorization;
+import com.iexec.sms.iexecsms.authorization.AuthorizationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,24 +18,40 @@ import java.util.Optional;
 public class SecretController {
 
     private SecretService secretService;
+    private AuthorizationService authorizationService;
 
-    public SecretController(SecretService secretService) {
+    public SecretController(SecretService secretService,
+                            AuthorizationService authorizationService) {
         this.secretService = secretService;
+        this.authorizationService = authorizationService;
     }
 
     /**
      * Used by the NON Tee workflow:
      * https://github.com/iExecBlockchainComputing/SMS/blob/tee-scone-autonome/python/daemon.py#L280
-     *
+     * <p>
      * Note
      * Blockchain checks are already made here:
      * `authorizationService.isAuthorizedToGetKeys(authorization)` (@ResponseBody Authorization authorization)
      * `iexecHubService.isTeeTask(chainTaskId)`
-     * */
+     *
+     * @return
+     */
     @GetMapping("/secrets/{address}")
-    public ResponseEntity<Secret> getSecret(@RequestParam String address, @RequestBody SmsRequest smsRequest) {
+    public ResponseEntity getSecret(@RequestParam String address, @RequestBody SmsRequest smsRequest) {
 
-        // TODO: check that the request is legitimate with all signatures and authorization on the blockchain
+        // Check that the demand is legitimate
+        SmsRequestData data = smsRequest.getSmsSecretRequestData();
+        Authorization authorization = Authorization.builder()
+                .chainTaskId(data.getChainTaskId())
+                .enclaveAddress(data.getEnclaveChallenge())
+                .workerAddress(data.getWorkerAddress())
+                .workerSignature(new Signature(data.getWorkerSignature()))
+                .workerpoolSignature(new Signature(data.getCoreSignature())).build();
+
+        if (!authorizationService.isAuthorizedToGetKeys(authorization)) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
 
         Optional<Secret> secret = secretService.getSecret(address);
         return secret.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
@@ -39,7 +60,7 @@ public class SecretController {
 
     @PostMapping("/secrets/{address}")
     public ResponseEntity setSecret(@RequestParam String address, @RequestBody SecretPayload secretPayload) {
-        // TODO: there should be a signature from the sender to check that it is correct
+        // TODO: should there be a signature from the sender to check that it is correct ?
 
         boolean isSecretSet = secretService.setSecret(Secret.builder().address(address).payload(secretPayload).build());
         if (isSecretSet) {
