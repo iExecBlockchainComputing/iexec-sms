45809d98497bae6277258238eeef41b474eef94c
Zied <26070035+zguesmi@users.noreply.github.com>
2020-04-17 18:53:35 +0200

    Fix feign client config

--- src/main/java/com/iexec/sms/tee/session/TeeSessionClient.java
+++ src/main/java/com/iexec/sms/tee/session/TeeSessionClient.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import feign.FeignException;
 
-@FeignClient(url = "#{teeCasConfiguration.getCasUrl()}")
+@FeignClient(name = "teeSessionClient", url = "#{teeCasConfiguration.getCasUrl()}")
 public interface TeeSessionClient {
 
-    @PostMapping("/session")
+    @PostMapping(value = "/session", consumes = "application/x-www-form-urlencoded")
     public ResponseEntity<String> generateSecureSession(@RequestBody byte[] palaemonFile,
             @RequestHeader(name = "Content-Type", required = false, defaultValue = "application/x-www-form-urlencoded") String contentType,
             @RequestHeader(name = "Expect", required = false, defaultValue = "100-continue") String expect) throws FeignException;
