1944b28dbea9025b131b4ab96219d5113356681e
mcornaton <maxence.cornaton@iexe.ec>
2022-08-24 15:06:54 +0200

    Get SMS URL from ChainDeal if task not initialized

--- iexec-sms-library/src/main/java/com/iexec/sms/api/SmsClientProvider.java
+++ iexec-sms-library/src/main/java/com/iexec/sms/api/SmsClientProvider.java
@@ -1,5 +1,6 @@
 package com.iexec.sms.api;
 
+import com.iexec.common.chain.ChainDeal;
 import com.iexec.common.chain.IexecHubAbstractService;
 import com.iexec.common.task.TaskDescription;
 import feign.Logger;
@@ -8,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Manages the {@link SmsClient}, providing an easy way to access SMS
@@ -15,6 +17,8 @@ import java.util.Map;
  */
 @Slf4j
 public class SmsClientProvider {
+    // TODO: purge once task has been completed
+    private final Map<String, Optional<String>> taskIdToSmsUrl = new HashMap<>();
     private final Map<String, SmsClient> urlToSmsClient = new HashMap<>();
 
     private final IexecHubAbstractService iexecHubService;
@@ -40,19 +44,46 @@ public class SmsClientProvider {
      * </ul>
      *
      * @param chainTaskId ID of the task the specified SMS URL should be retrieved.
+     * @throws SmsClientCreationException if SMS URL can't be retrieved.
      * @return An instance of {@link SmsClient} pointing on the task's specified SMS.
      */
     public SmsClient getOrCreateSmsClientForTask(String chainTaskId) {
-        final TaskDescription taskDescription = iexecHubService.getTaskDescription(chainTaskId);
-        if (taskDescription == null) {
-            throw new SmsClientCreationException("No such task [chainTaskId: " + chainTaskId +"]");
-        }
-
-        final String smsUrl = taskDescription.getSmsUrl();
-        if (StringUtils.isEmpty(smsUrl)) {
+        final Optional<String> smsUrl = getSmsUrlForTask(chainTaskId);
+        if (smsUrl.isEmpty() || StringUtils.isEmpty(smsUrl.get())) {
             throw new SmsClientCreationException("No SMS URL defined for given task [chainTaskId: " + chainTaskId +"]");
         }
 
-        return urlToSmsClient.computeIfAbsent(smsUrl, url -> SmsClientBuilder.getInstance(loggerLevel, url));
+        return urlToSmsClient.computeIfAbsent(smsUrl.get(), url -> SmsClientBuilder.getInstance(loggerLevel, url));
+    }
+
+    /**
+     * Retrieves the SMS URL defined for this task and caches the result.
+     * </p>
+     * If the task has already been initialized, then gets the URL from the {@link TaskDescription}.
+     * Otherwise, gets the URL from the {@link ChainDeal}.
+     *
+     * @param chainTaskId ID of the task the specified SMS URL should be retrieved.
+     * @return The SMS URL defined for this task.
+     */
+    Optional<String> getSmsUrlForTask(String chainTaskId) {
+        if (taskIdToSmsUrl.containsKey(chainTaskId)) {
+            return taskIdToSmsUrl.get(chainTaskId);
+        }
+
+        final Optional<String> smsUrl;
+
+        final TaskDescription taskDescription = iexecHubService.getTaskDescription(chainTaskId);
+        if (taskDescription != null) {
+            smsUrl = Optional.ofNullable(taskDescription.getSmsUrl());
+        } else {
+            // Fallback: if task is not initialized yet,
+            // we can still get its SMS url in its deal.
+            final Optional<ChainDeal> chainDeal = iexecHubService.getChainDeal(chainTaskId);
+            smsUrl = chainDeal
+                    .map(deal -> deal.getParams().getIexecSmsUrl());
+        }
+
+        taskIdToSmsUrl.put(chainTaskId, smsUrl);
+        return smsUrl;
     }
 }
--- iexec-sms-library/src/test/java/com/iexec/sms/api/SmsClientProviderTests.java
+++ iexec-sms-library/src/test/java/com/iexec/sms/api/SmsClientProviderTests.java
@@ -1,5 +1,7 @@
 package com.iexec.sms.api;
 
+import com.iexec.common.chain.ChainDeal;
+import com.iexec.common.chain.DealParams;
 import com.iexec.common.chain.IexecHubAbstractService;
 import com.iexec.common.task.TaskDescription;
 import feign.Logger;
@@ -9,7 +11,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class SmsClientProviderTests {
     private static final String CHAIN_TASK_ID_1 = "chainTaskId1";
@@ -26,11 +31,7 @@ class SmsClientProviderTests {
             .smsUrl(SMS_URL_2)
             .build();
 
-    private static final String CHAIN_TASK_ID_3 = "chainTaskId1";
-    private static final TaskDescription TASK_DESCRIPTION_3 = TaskDescription.builder()
-            .chainTaskId(CHAIN_TASK_ID_3)
-            .smsUrl(SMS_URL_1)  // Same as first task
-            .build();
+    private static final String CHAIN_TASK_ID_3 = "chainTaskId3";
 
     @Mock
     IexecHubAbstractService iexecHubService;
@@ -40,12 +41,13 @@ class SmsClientProviderTests {
     @BeforeEach
     void init() {
         MockitoAnnotations.openMocks(this);
-        smsClientProvider = new SmsClientProvider(iexecHubService, Logger.Level.NONE);
+        smsClientProvider = spy(new SmsClientProvider(iexecHubService, Logger.Level.NONE));
     }
 
+    // region getOrCreateSmsClientForTask
     @Test
     void shouldGetSmsClientForTask() {
-        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(TASK_DESCRIPTION_1);
+        Mockito.when(smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1)).thenReturn(Optional.of(SMS_URL_1));
 
         final SmsClient smsClient = assertDoesNotThrow(() -> smsClientProvider.getOrCreateSmsClientForTask(CHAIN_TASK_ID_1));
         assertNotNull(smsClient);
@@ -53,7 +55,7 @@ class SmsClientProviderTests {
 
     @Test
     void shouldNotRebuildSmsClientForSameTask() {
-        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(TASK_DESCRIPTION_1);
+        Mockito.when(smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1)).thenReturn(Optional.of(SMS_URL_1));
 
         final SmsClient smsClient1 = assertDoesNotThrow(() -> smsClientProvider.getOrCreateSmsClientForTask(CHAIN_TASK_ID_1));
         assertNotNull(smsClient1);
@@ -66,11 +68,11 @@ class SmsClientProviderTests {
 
     @Test
     void shouldNotRebuildSmsClientForSameSmsUrlOnAnotherTask() {
-        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(TASK_DESCRIPTION_1);
+        Mockito.when(smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1)).thenReturn(Optional.of(SMS_URL_1));
         final SmsClient smsClient1 = assertDoesNotThrow(() -> smsClientProvider.getOrCreateSmsClientForTask(CHAIN_TASK_ID_1));
         assertNotNull(smsClient1);
 
-        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_3)).thenReturn(TASK_DESCRIPTION_3);
+        Mockito.when(smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_3)).thenReturn(Optional.of(SMS_URL_1));
         final SmsClient smsClient2 = assertDoesNotThrow(() -> smsClientProvider.getOrCreateSmsClientForTask(CHAIN_TASK_ID_3));
         assertNotNull(smsClient2);
 
@@ -79,8 +81,8 @@ class SmsClientProviderTests {
 
     @Test
     void shouldBuildAnotherSmsClientForTaskOnSecondCallForAnotherSms() {
-        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(TASK_DESCRIPTION_1);
-        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_2)).thenReturn(TASK_DESCRIPTION_2);
+        Mockito.when(smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1)).thenReturn(Optional.of(SMS_URL_1));
+        Mockito.when(smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_2)).thenReturn(Optional.of(SMS_URL_2));
 
         final SmsClient smsClientForTask1 = assertDoesNotThrow(() -> smsClientProvider.getOrCreateSmsClientForTask(CHAIN_TASK_ID_1));
         assertNotNull(smsClientForTask1);
@@ -88,25 +90,97 @@ class SmsClientProviderTests {
         final SmsClient smsClientForTask2 = assertDoesNotThrow(() -> smsClientProvider.getOrCreateSmsClientForTask(CHAIN_TASK_ID_2));
         assertNotNull(smsClientForTask2);
 
-
         assertNotEquals(smsClientForTask1, smsClientForTask2);
     }
 
     @Test
-    void shouldNotGetSmsClientForTaskWhenNoTask() {
-        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(null);
-
-        SmsClientCreationException e = assertThrows(SmsClientCreationException.class,
-                () -> smsClientProvider.getOrCreateSmsClientForTask(CHAIN_TASK_ID_1));
-        assertEquals("No such task [chainTaskId: " + CHAIN_TASK_ID_1 +"]", e.getMessage());
-    }
-
-    @Test
     void shouldNotGetSmsClientForTaskWhenNoSmsUrl() {
-        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(TaskDescription.builder().build());
+        Mockito.when(smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1)).thenReturn(Optional.empty());
 
         SmsClientCreationException e = assertThrows(SmsClientCreationException.class,
                 () -> smsClientProvider.getOrCreateSmsClientForTask(CHAIN_TASK_ID_1));
         assertEquals("No SMS URL defined for given task [chainTaskId: " + CHAIN_TASK_ID_1 +"]", e.getMessage());
     }
+    // endregion
+
+    // region getSmsUrlForTask
+    @Test
+    void shouldGetSmsUrlForTaskWhenTaskFound() {
+        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(TASK_DESCRIPTION_1);
+
+        Optional<String> smsUrl = smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1);
+        assertTrue(smsUrl.isPresent());
+        assertEquals(SMS_URL_1, smsUrl.get());
+
+        verify(iexecHubService, times(1)).getTaskDescription(any());
+        verify(iexecHubService, times(0)).getChainDeal(any());
+    }
+
+    @Test
+    void shouldGetSmsUrlOnlyOnceForSameTask() {
+        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(TASK_DESCRIPTION_1);
+
+        Optional<String> smsUrlFirstCall = smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1);
+        assertTrue(smsUrlFirstCall.isPresent());
+        assertEquals(SMS_URL_1, smsUrlFirstCall.get());
+
+        verify(iexecHubService, times(1)).getTaskDescription(any());
+        verify(iexecHubService, times(0)).getChainDeal(any());
+
+        Optional<String> smsUrlSecondCall = smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1);
+        assertTrue(smsUrlSecondCall.isPresent());
+        assertEquals(SMS_URL_1, smsUrlSecondCall.get());
+
+        verify(iexecHubService, times(1)).getTaskDescription(any());
+        verify(iexecHubService, times(0)).getChainDeal(any());
+    }
+
+    @Test
+    void shouldGetSmsUrlTwiceForDifferentTasks() {
+        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(TASK_DESCRIPTION_1);
+        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_2)).thenReturn(TASK_DESCRIPTION_2);
+
+        Optional<String> smsUrlFirstCall = smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1);
+        assertTrue(smsUrlFirstCall.isPresent());
+        assertEquals(SMS_URL_1, smsUrlFirstCall.get());
+
+        verify(iexecHubService, times(1)).getTaskDescription(any());
+        verify(iexecHubService, times(0)).getChainDeal(any());
+
+        Optional<String> smsUrlSecondCall = smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_2);
+        assertTrue(smsUrlSecondCall.isPresent());
+        assertEquals(SMS_URL_2, smsUrlSecondCall.get());
+
+        verify(iexecHubService, times(2)).getTaskDescription(any());
+        verify(iexecHubService, times(0)).getChainDeal(any());
+    }
+
+
+    @Test
+    void shouldGetSmsUrlForTaskWhenTaskNotFoundButDealFound() {
+        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(null);
+        Mockito.when(iexecHubService.getChainDeal(CHAIN_TASK_ID_1)).thenReturn(Optional.of(
+                ChainDeal.builder().params(DealParams.builder().iexecSmsUrl(SMS_URL_1).build()).build()
+        ));
+
+        Optional<String> smsUrl = smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1);
+        assertTrue(smsUrl.isPresent());
+        assertEquals(SMS_URL_1, smsUrl.get());
+
+        verify(iexecHubService, times(1)).getTaskDescription(any());
+        verify(iexecHubService, times(1)).getChainDeal(any());
+    }
+
+    @Test
+    void shouldNotGetSmsUrlForTaskWhenTaskAndDealNotFound() {
+        Mockito.when(iexecHubService.getTaskDescription(CHAIN_TASK_ID_1)).thenReturn(null);
+        Mockito.when(iexecHubService.getChainDeal(CHAIN_TASK_ID_1)).thenReturn(Optional.empty());
+
+        Optional<String> smsUrl = smsClientProvider.getSmsUrlForTask(CHAIN_TASK_ID_1);
+        assertTrue(smsUrl.isEmpty());
+
+        verify(iexecHubService, times(1)).getTaskDescription(any());
+        verify(iexecHubService, times(1)).getChainDeal(any());
+    }
+    // region
 }
