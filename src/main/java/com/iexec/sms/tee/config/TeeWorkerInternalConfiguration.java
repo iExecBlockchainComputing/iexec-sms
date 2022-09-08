1113289c34e83d446d6f8a797e63f5fae063c9fc
Maxence Cornaton <maxence.cornaton@iex.ec>
2022-09-08 14:56:20 +0200

    Fix pre/post-compute properties validation

--- src/main/java/com/iexec/sms/tee/config/TeeWorkerInternalConfiguration.java
+++ src/main/java/com/iexec/sms/tee/config/TeeWorkerInternalConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.unit.DataSize;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 
 @Configuration
+@Validated
 public class TeeWorkerInternalConfiguration {
     @Bean
     TeeAppProperties preComputeConfiguration(
