eac272d27152179dee8e0039ee2f529c5caac2ae
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-11-15 09:17:08 +0100

    Set EncryptionConfiguration immutable

--- src/main/java/com/iexec/sms/App.java
+++ src/main/java/com/iexec/sms/App.java
@@ -18,8 +18,10 @@ package com.iexec.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class App {
 
     public static final String DOMAIN = "IEXEC_SMS_DOMAIN"; // TODO: Add session salt after domain
