046319b32d73f4fa272c59668e331eab6b957c9f
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-07-27 14:29:31 +0200

    Add tests for secrets metrics

--- src/test/java/com/iexec/sms/secret/web2/Web2SecretServiceTests.java
+++ src/test/java/com/iexec/sms/secret/web2/Web2SecretServiceTests.java
@@ -19,8 +19,16 @@
 package com.iexec.sms.secret.web2;
 
 import com.iexec.sms.encryption.EncryptionService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -36,6 +44,7 @@ class Web2SecretServiceTests {
     private static final String SECRET_ADDRESS = "secretAddress";
     private static final String PLAIN_SECRET_VALUE = "plainSecretValue";
     private static final String ENCRYPTED_SECRET_VALUE = "encryptedSecretValue";
+    private static final String METRICS_PREFIX = "iexec.sms.secrets.web2.";
 
     @Mock
     private Web2SecretRepository web2SecretRepository;
@@ -46,10 +55,33 @@ class Web2SecretServiceTests {
     @InjectMocks
     private Web2SecretService web2SecretService;
 
+    private MeterRegistry meterRegistry;
+
     @BeforeEach
     void beforeEach() {
+        meterRegistry = new SimpleMeterRegistry();
+        Metrics.globalRegistry.add(meterRegistry);
+
         MockitoAnnotations.openMocks(this);
     }
+
+    @AfterEach
+    void afterEach() {
+        meterRegistry.clear();
+        Metrics.globalRegistry.clear();
+    }
+
+    // region init
+    @Test
+    void shouldRegisterCounter() {
+        final long initialCount = 5L;
+        when(web2SecretRepository.count()).thenReturn(initialCount);
+
+        web2SecretService.init();
+
+        assertInitialCount(initialCount);
+    }
+    // endregion
 
     // region getSecret
     @Test
@@ -118,6 +150,7 @@ class Web2SecretServiceTests {
         assertAll(
                 () -> assertThat(newSecret).extracting(Web2Secret::getHeader).usingRecursiveComparison().isEqualTo(new Web2SecretHeader(OWNER_ADDRESS, SECRET_ADDRESS)),
                 () -> assertThat(newSecret).extracting(Web2Secret::getValue).isEqualTo(ENCRYPTED_SECRET_VALUE),
+                () -> assertAddedCount(1),
 
                 () -> verify(encryptionService).encrypt(any()),
                 () -> verify(web2SecretRepository).save(any())
@@ -135,6 +168,8 @@ class Web2SecretServiceTests {
         assertAll(
                 () -> assertEquals(OWNER_ADDRESS, exception.getOwnerAddress()),
                 () -> assertEquals(SECRET_ADDRESS, exception.getSecretAddress()),
+                () -> assertAddedCount(0),
+
                 () -> verify(encryptionService, never()).encrypt(PLAIN_SECRET_VALUE),
                 () -> verify(web2SecretRepository, never()).save(any())
         );
@@ -194,6 +229,47 @@ class Web2SecretServiceTests {
                 () -> assertEquals(SECRET_ADDRESS, exception.getSecretAddress()),
                 () -> verify(web2SecretRepository, never()).save(any())
         );
+    }
+    // endregion
+
+    // region stored count
+    @ParameterizedTest
+    @ValueSource(longs = {-1, 0, 10, Long.MAX_VALUE})
+    void storedCount(long expectedCount) {
+        when(web2SecretRepository.count()).thenReturn(expectedCount);
+
+        assertStoredCount(expectedCount);
+
+        verify(web2SecretRepository).count();
+    }
+    // endregion
+
+    // region utils
+    void assertInitialCount(long expectedCount) {
+        final Counter initialCounter = meterRegistry.find(METRICS_PREFIX + "initial")
+                .counter();
+
+        assertThat(initialCounter)
+                .extracting(Counter::count)
+                .isEqualTo((double) expectedCount);
+    }
+
+    void assertAddedCount(long expectedCount) {
+        final Counter addedCounter = meterRegistry.find(METRICS_PREFIX + "added")
+                .counter();
+
+        assertThat(addedCounter)
+                .extracting(Counter::count)
+                .isEqualTo((double) expectedCount);
+    }
+
+    void assertStoredCount(long expectedCount) {
+        final Gauge storedGauge = meterRegistry.find(METRICS_PREFIX + "stored")
+                .gauge();
+
+        assertThat(storedGauge)
+                .extracting(Gauge::value)
+                .isEqualTo((double) expectedCount);
     }
     // endregion
 }
