28b484e47e2cfa43d8c6a59c82d0e65764a499d5
Jeremy Bernard <jeremy.bernard@iex.ec>
2025-02-17 11:01:23 +0100

    Replace TeeServicesProperties beans with new Map in integration tests (#291)

--- src/itest/java/com/iexec/sms/tee/GramineBeansLoadingTests.java
+++ src/itest/java/com/iexec/sms/tee/GramineBeansLoadingTests.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2022-2025 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.iexec.sms.tee;
 
-import com.iexec.sms.api.config.GramineServicesProperties;
+import com.iexec.sms.api.config.TeeServicesProperties;
 import com.iexec.sms.tee.session.gramine.GramineSessionHandlerService;
 import com.iexec.sms.tee.session.gramine.GramineSessionMakerService;
 import com.iexec.sms.tee.session.gramine.sps.SpsConfiguration;
@@ -10,11 +26,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Map;
 
 @ActiveProfiles(profiles = {"gramine", "test"})
 public class GramineBeansLoadingTests extends TeeBeansLoadingTests {
     @Autowired
-    GramineServicesProperties gramineServicesProperties;
+    Map<String, TeeServicesProperties> gramineServicesProperties;
     @Autowired
     GramineSessionHandlerService gramineSessionHandlerService;
     @Autowired
